d6b8c3c31f85fe10b2c05eef49e6870e1434fff5
IvanPrikaznov <prikaznov555@mail.ru>
2023-10-22 18:08:07 +0200

    separete handle from routes

--- src/wryb/core.clj
+++ src/wryb/core.clj
@@ -1,51 +1,15 @@
 (ns wryb.core
   (:require
 ;;   [wryb.domain.taskrepo :as repo]
-   [wryb.domain.sqlite.taskrepo :as repo]
-   [clojure.string :refer [join]]
+   [wryb.router :as router]
    [compojure.core :refer [defroutes GET POST DELETE]]
-   [ring.middleware.resource :refer [wrap-resource]]
-   [ring.util.request :refer [body-string]]
-   [ring.util.response :refer [content-type resource-response response]]
-   [wryb.domain.task :refer [task-from-json task-to-json]]
-   [clojure.data.json :as json]))
-
-(defn- store-task! [request]
-  (let [new-task (-> (body-string request)
-                     (task-from-json))]
-    (task-to-json (repo/save! new-task))))
-
-(defn- remove-task! [request]
-  (let [id (-> (body-string request)
-               (json/read-str)
-               (get "id"))]
-    (repo/remove! id)
-    id))
-
-(defn- logging! [request]
-  (println request)
-  request)
-
-(defn- return-task-list []
-  (let [data-list (repo/get-all)]
-    (if (empty? data-list) nil
-        (concat "[" (join "," (map (fn [el] (task-to-json el)) data-list)) "]"))))
-
-(defn- app-json [resp]
-  (content-type resp "application/json"))
+   [ring.middleware.resource :refer [wrap-resource]]))
 
 (defroutes routes
-  (GET "/" [] (resource-response "public/index.html"))
-  (GET "/tasks" [] (app-json (response (return-task-list))))
-  (POST "/task" request (-> request
-                            ;;(logging!)
-                            (store-task!)
-                            (response)
-                            (app-json)))
-  (DELETE "/task" request (-> request
-                              (logging!)
-                              (remove-task!)
-                              (response))))
+  (GET "/" [] (router/handle-mainpage))
+  (GET "/tasks" [] (router/handle-tasks))
+  (POST "/task" request (router/handle-store-task! request))
+  (DELETE "/task" request (router/handle-remove-task! request)))
 
 (def app
   (-> routes
---
+++ src/wryb/router.clj
@@ -0,0 +1,49 @@
+(ns wryb.router
+  (:require
+   [clojure.string :refer [join]]
+   [wryb.domain.sqlite.taskrepo :as repo]
+   [ring.util.request :refer [body-string]]
+   [wryb.domain.task :refer [task-from-json task-to-json]]
+   [ring.util.response :refer [content-type resource-response response]]
+   [clojure.data.json :as json]))
+
+(defn- return-task-list []
+  (let [data-list (repo/get-all)]
+    (if (empty? data-list) nil
+        (concat "[" (join "," (map (fn [el] (task-to-json el)) data-list)) "]"))))
+
+(defn- app-json [resp]
+  (content-type resp "application/json"))
+
+(defn- logging! [request]
+  (println request)
+  request)
+
+(defn- remove-task! [request]
+  (let [id (-> (body-string request)
+               (json/read-str)
+               (get "id"))]
+    (repo/remove! id)
+    id))
+
+(defn- store-task! [request]
+  (let [new-task (-> (body-string request)
+                     (task-from-json))]
+    (task-to-json (repo/save! new-task))))
+
+(defn handle-mainpage []
+  (resource-response "public/index.html"))
+
+(defn handle-store-task! [request]
+  (-> request
+      (store-task!)
+      (response)
+      (app-json)))
+
+(defn handle-remove-task! [request]
+  (-> request
+      (remove-task!)
+      (response)))
+
+(defn handle-tasks []
+  (app-json (response (return-task-list))))
