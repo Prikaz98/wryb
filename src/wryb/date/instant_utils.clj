ce1899c735daaa05068c6bd6dd8e69151773430d
IvanPrikaznov <prikaznov555@mail.ru>
2023-11-05 10:57:19 +0100

    instant to timestamp

--- src/wryb/date/instant_utils.clj
+++ src/wryb/date/instant_utils.clj
@@ -1,11 +1,18 @@
 (ns wryb.date.instant-utils
   (:import (java.time Instant)
-           (java.time.format DateTimeFormatter)))
+           (java.time.format DateTimeFormatter)
+           (java.sql Timestamp)))
 
 (defn now [] (Instant/now))
 
-(defn parse
+(defn parse-instant
   ([instant-str] (Instant/from (.parse DateTimeFormatter/ISO_INSTANT instant-str))))
 
-(defn format
+(defn format-instant
   ([instant] (.format DateTimeFormatter/ISO_INSTANT instant)))
+
+(defn instant-to-timestamp [instant]
+  (Timestamp/from instant))
+
+(defn timestamp-to-instant [timestamp]
+  (.toInstant timestamp))
